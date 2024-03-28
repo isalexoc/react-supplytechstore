b31472b1100fc05dfa6636afd50ab9f502eeeecb
Isaac Orraiz <isaacorraiz@gmail.com>
2024-03-28 13:05:38 -0400

    Bugs fixed

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,12 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-const port = process.env.PORT || 5000;
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+
+const port = process.env.PORT || 5000;
 
 connectDB();
 
