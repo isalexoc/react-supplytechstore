8f6bee6d01787a1ffe7f5202d4e1a4d5c54993bc
Isaac Orraiz <isaacorraiz@gmail.com>
2024-04-14 20:56:52 -0400

    og head

--- frontend/src/components/Meta.jsx
+++ frontend/src/components/Meta.jsx
@@ -1,5 +1,4 @@
 import { Helmet } from "react-helmet-async";
-import { useState } from "react";
 
 const Meta = ({
   title,
@@ -9,35 +8,14 @@ const Meta = ({
   contentDescription,
   contentTitle,
 }) => {
-  const [image, setImage] = useState(
-    "https://www.supplytechstore.com/images/logoog.jpg"
-  );
-  const [contentDescriptionState, setContentDescriptionState] = useState(
-    "Materiales especializados en soldadura, corte, medición, seguridad industrial, y ferreteria."
-  );
-  const [propertyTitleState, setPropertyTitleState] =
-    useState("SupplyTechStore");
-
-  if (contentImage) {
-    setImage(contentImage);
-  }
-
-  if (contentDescription) {
-    setContentDescriptionState(contentDescription);
-  }
-
-  if (contentTitle) {
-    setPropertyTitleState(contentTitle);
-  }
-
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keyword" content={keywords} />
-      <meta property="og:image" content={image} />
-      <meta property="og:description" content={contentDescriptionState} />
-      <meta property="og:title" content={propertyTitleState} />
+      <meta property="og:image" content={contentImage || ""} />
+      <meta property="og:description" content={contentDescription || ""} />
+      <meta property="og:title" content={contentTitle || ""} />
     </Helmet>
   );
 };
