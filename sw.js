981484394aca7492a7e1237bc928e6a7c27a95ee
Isaac Orraiz <isaacorraiz@gmail.com>
2024-05-05 15:36:06 -0400

    working in the service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener("fetch", (event) => {
   console.log("WORKER: Fetching", event.request);
 });
+
+self.addEventListener("install", (event) => {
+  console.log("WORKER: install event in progress.");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("WORKER: activate event in progress.");
+});
