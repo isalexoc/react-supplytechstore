6f9d7be0d7b1a8152068ad85c57d2a8082136739
Isaac Orraiz <isaacorraiz@gmail.com>
2024-05-05 16:16:13 -0400

    working in the service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,9 +1,42 @@
-self.addEventListener("fetch", (event) => {
-  console.log("WORKER: Fetching", event.request);
+const CACHE_NAME = "sts-cache";
+// The list of static files your app needs to start.
+const PRE_CACHED_RESOURCES = ["/"];
+
+self.addEventListener("fetch", event => {
+  async function returnCachedResource() {
+    // Open the app's cache.
+    const cache = await caches.open(CACHE_NAME);
+    // Find the response that was pre-cached during the `install` event.
+    const cachedResponse = await cache.match(event.request.url);
+
+    if (cachedResponse) {
+      // Return the resource.
+      return cachedResponse;
+    } else {
+      // The resource wasn't found in the cache, so fetch it from the network.
+      const fetchResponse = await fetch(event.request.url);
+      // Put the response in cache.
+      cache.put(event.request.url, fetchResponse.clone());
+      // And return the response.
+      return fetchResponse.
+    }
+  }
+
+  event.respondWith(returnCachedResource());
 });
 
+
+
+// Listen to the `install` event.
 self.addEventListener("install", (event) => {
-  console.log("WORKER: install event in progress.");
+  async function preCacheResources() {
+    // Open the app's cache.
+    const cache = await caches.open(CACHE_NAME);
+    // Cache all static resources.
+    cache.addAll(PRE_CACHED_RESOURCES);
+  }
+
+  event.waitUntil(preCacheResources());
 });
 
 self.addEventListener("activate", (event) => {
