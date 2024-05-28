3c5e185d731ca9ef0498c2baed7a689009fe62e4
Isaac Orraiz <isaacorraiz@gmail.com>
2024-05-27 20:37:42 -0400

    Donwload Catalog

--- frontend/src/screens/Catalog.jsx
+++ frontend/src/screens/Catalog.jsx
@@ -98,7 +98,7 @@ const Catalog = () => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "catalog.pdf");
+        link.setAttribute("download", "productos-supplytechstore.pdf");
         document.body.appendChild(link);
         link.click();
         link.parentNode.removeChild(link);
